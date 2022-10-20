30c6d1e3b2869b66b2ecd7754eb8cda82a8459fb
Thomas Nguyen <ttn@ou.edu>
2022-10-19 20:33:20 -0500

    Complete prototype

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/game'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Game />
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ttn729/chessColors"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
